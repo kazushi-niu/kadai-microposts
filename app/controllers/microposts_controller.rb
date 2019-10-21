16aee86494801f3f6191234570f43833f3bdf5a8
kazushi.niu <kaz.niu@gmail.com>
2019-10-21 01:28:26 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -13,7 +13,7 @@ class MicropostsController < ApplicationController
       redirect_to root_url
     else
       #投稿失敗
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -38,4 +38,9 @@ class User < ApplicationRecord
     #フォローしている中に存在しているか
     self.followings.include?(other_user)
   end
+  
+  #タイムライン用のmicropostを取得
+  def feed_microposts
+    Micropost.where(user_id: self.following_ids + [self.id])
+  end
 end
